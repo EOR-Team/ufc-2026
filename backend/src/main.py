3f10fbcd5bdc0fd999eacc7977860418cd07adf7
n1ghts4kura <1825259836@qq.com>
2026-02-07 13:00:18 +0800

    后端初始化

---
+++ backend/src/main.py
@@ -0,0 +1,18 @@
+# main.py
+# 后端服务器入口
+#
+
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+root_router = APIRouter(prefix="/api")
