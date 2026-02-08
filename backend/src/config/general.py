b4a44fe858b9ab987b9f4f054b9afb1d8311d68d
n1ghts4kura <1825259836@qq.com>
2026-02-08 10:09:20 +0800

    添加本地embed服务

--- backend/src/config/general.py
+++ backend/src/config/general.py
@@ -20,16 +20,20 @@ OFFLINE_MODEL_TYPES = Literal[
     "qwen2.5coder-1.5b",
     "qwen2.5coder-3b",
     "qwen3-1.7b",
-    "qwen3-4b"
+    "qwen3-4b",
+    "qwen3-embed-0.6b"
 ]
 
-DEFAULT_OFFLINE_MODEL: OFFLINE_MODEL_TYPES = "qwen2.5coder-1.5b"
+DEFAULT_OFFLINE_CHAT_MODEL: OFFLINE_MODEL_TYPES = "qwen2.5coder-1.5b"
+
+DEFAULT_OFFLINE_EMBED_MODEL: OFFLINE_MODEL_TYPES = "qwen3-embed-0.6b"
 
 OFFLINE_MODEL_HOST = "http://localhost:8080/v1"
 
 OFFLINE_MODEL_CTX_LEN = 8192
 
 OFFLINE_MODEL_THREADS = os.cpu_count() or 4
+
 
 # === 在线 ===
 
--- backend/src/llm/offline/server.py
+++ backend/src/llm/offline/server.py
@@ -1,5 +1,5 @@
 # llm/local/server.py
-# 本地 LLM 服务
+# 本地 LLM 相关 服务
 #
 
 import time
@@ -9,7 +9,8 @@ import subprocess
 from src.config import general
 
 
-_server_process: subprocess.Popen | None = None
+_server_process_chat: subprocess.Popen | None = None
+_server_process_embed: subprocess.Popen | None = None
 
 
 def _convert_model_name_to_filepath(model_name: str) -> str:
@@ -27,16 +28,16 @@ def _convert_model_name_to_filepath(model_name: str) -> str:
     return (general.OFFLINE_MODEL_DIR / filename).as_posix()
     
 
-def start_local_llm_server() -> bool:
+def start_local_chat_server() -> bool:
     """
     启动本地 LLM 服务
     """
 
-    global _server_process
-    if _server_process is not None:
+    global _server_process_chat
+    if _server_process_chat is not None:
         return True  # 已经启动
 
-    model_filepath = _convert_model_name_to_filepath(general.DEFAULT_OFFLINE_MODEL)
+    model_filepath = _convert_model_name_to_filepath(general.DEFAULT_OFFLINE_CHAT_MODEL)
     if not model_filepath:
         return False  # 模型文件不存在
 
@@ -46,33 +47,123 @@ def start_local_llm_server() -> bool:
         "--model", model_filepath,
         "--n_gpu_layers", "0",
         "--n_ctx", str(general.OFFLINE_MODEL_CTX_LEN),
-        "--n_threads", str(general.OFFLINE_MODEL_THREADS),
+        "--n_threads", str(general.OFFLINE_MODEL_THREADS - 1), # 留一个线程给 Embedding 服务
         "--verbose", "False",
         "--host", "0.0.0.0",
         "--port", "8080"
     ]
 
-    _server_process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _server_process_chat = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     time.sleep(5)  # 等待服务启动
 
-    if _server_process.poll() is not None:
-        _server_process = None
+    if _server_process_chat.poll() is not None:
+        _server_process_chat = None
         return False  # 启动失败
+
+    return True
+
+
+def start_local_embed_server() -> bool:
+    """
+    启动本地 Embedding 服务
+    """
+
+    global _server_process_embed
+    if _server_process_embed is not None:
+        return True  # 已经启动
+
+    model_filepath = _convert_model_name_to_filepath(general.DEFAULT_OFFLINE_EMBED_MODEL)
+    if not model_filepath:
+        return False  # 模型文件不存在
+
+    command = [
+        (general.BACKEND_ROOT_DIR / "venv" / "bin" / "python").as_posix(),
+        "-m", "llama_cpp.server",
+        "--model", model_filepath,
+        "--n_gpu_layers", "0",
+        "--n_ctx", str(general.OFFLINE_MODEL_CTX_LEN),
+        "--n_threads", "1", # Embedding 服务只用一个线程
+        "--verbose", "False",
+        "--host", "0.0.0.0",
+        "--port", "8081"
+    ]
+
+    _server_process_embed = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    time.sleep(5)  # 等待服务启动
+
+    if _server_process_embed.poll() is not None:
+        _server_process_embed = None
+        return False  # 启动失败
+
+    return True
+
+
+def stop_local_chat_server() -> bool:
+    """
+    停止本地 LLM 服务
+    """
+
+    global _server_process_chat
+    if _server_process_chat is None:
+        return True  # 未启动
+
+    _server_process_chat.terminate()
+    _server_process_chat = None
+
+    return True
+
+
+def stop_local_embed_server() -> bool:
+    """
+    停止本地 Embedding 服务
+    """
+
+    global _server_process_embed    
+    if _server_process_embed is None:
+        return True  # 未启动
+
+    _server_process_embed.terminate()
+    _server_process_embed = None
+
+    return True
+
+
+def start_local_llm_server() -> bool:
+    """
+    启动本地 LLM 服务和 Embedding 服务
+    """
+
+    print("Starting chat server...")
+    chat_started = start_local_chat_server()
+    if not chat_started:
+        print("Failed to start chat server!!")
+        return False
+
+    print("Starting embed server...")
+    embed_started = start_local_embed_server()
+    if not embed_started:
+        print("Failed to start embed server!!")
+        return False
 
     return True
 
 
 def stop_local_llm_server() -> bool:
     """
-    停止本地 LLM 服务
+    停止本地 LLM 服务和 Embedding 服务
     """
 
-    global _server_process
-    if _server_process is None:
-        return True  # 未启动
+    print("Stopping chat server...")
+    chat_stopped = stop_local_chat_server()
+    print("Stopping embed server...")
+    embed_stopped = stop_local_embed_server()
 
-    _server_process.terminate()
-    _server_process = None
+    return chat_stopped and embed_stopped
 
-    return True
+
+__all__ = [
+    "start_local_llm_server",
+    "stop_local_llm_server",
+]
